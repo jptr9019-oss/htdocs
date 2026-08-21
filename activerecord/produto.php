7978f1cf9e221a912e1b7bfa7eba4e22f3981573
jptr9019-oss <jptr9019@gmail.com>
2026-08-21 09:05:23 -0300

    terminando o save e o load esta feito

--- activerecord/produto.php
+++ activerecord/produto.php
@@ -41,7 +41,7 @@
         public function save(){
             $pdo = getConnection();
             if ($this->id){
-                $sql = "UPDATE produto SET nome=:n, preco=:p, descricao=:d, quantidade=:q WHERE id=:id";
+                $sql = "UPDATE produto SET nome=:n, preco=:p, descricao=:d, quantidade=:q WHERE id_prod=:id";
                 $stmt = $pdo->prepare($sql);
                 return $stmt->execute([
                     ':n' => $this->nome,
@@ -49,7 +49,34 @@
                     ':d' => $this->descricao,
                     ':q' => $this->quantidade
                 ]);
+            } else {
+                $sql = "INSERT INTO produto (nome, preco, descricao, quantidade) VALUES (:n, :p, :d, :q)";
+                $stmt = $pdo->prepare($sql);
+                $ok = $stmt->execute([
+                    ':n' => $this->nome,
+                    ':p' => $this->preco,
+                    ':d' => $this->descricao,
+                    ':q' => $this->quantidade
+                ]);
+                if($ok){
+                    $this->id = $this->pdo->lastInsertId();
+                }
+                return $ok;
             }
+        }
+        public function load($id){
+            $pdo = getConnection();
+            $stmt = $pdo->prepare("SELECT * FROM produto WHERE id_prod = :id");
+            $stmt->execute([':id' => $id]);
+            if($dados = $stmt->fetch(PDO::FETCH_ASSOC)){
+                $this->id = $dados['id_prod'];
+                $this->nome = $dados['nome'];
+                $this->preco = $dados['preco'];
+                $this->descricao = $dados['descricao'];
+                $this->quantidade = $dados['quantidade'];
+                return true;
+            }
+            return false;
         }
     }
 ?>
